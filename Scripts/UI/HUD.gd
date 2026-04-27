d3e967a4ab45fb3c726d4b870137e369cd732480
Yuna <rushikeshingale2001@gmail.com>
2026-04-27 18:24:27 +0530

    Added Inventory UI

---
+++ Scripts/UI/HUD.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+class_name HUD
+
+func _on_equipment_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_inventory_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_stats_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_skills_pressed() -> void:
+	pass # Replace with function body.
